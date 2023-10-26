cbd75a18f6a6e88b361bb7dcdbb4bcb52ec0a6f4
Marcelo Vergara <marcelorv@gmail.com>
2023-10-26 07:34:39 -0300

    Creating a Custom Asynchronous Validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -21,7 +22,7 @@ export class AppComponent implements OnInit {
       // Nested form group
       'userData': new FormGroup({
         'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email]),
+        'email': new FormControl(null, [Validators.required, Validators.email],this.forbiddenEmails),
       }),
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
@@ -45,5 +46,18 @@ export class AppComponent implements OnInit {
     }
     // Should return null if the control is ok
     return null
+  }
+
+  forbiddenEmails(controle: FormControl) : Promise<any> |Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if(controle.value === 'test@test.com'){
+          resolve({'emailIsForbidden': true})
+        } else {
+          resolve(null)
+        }
+      },1500)
+    })
+    return promise
   }
 }
